6dd8f96a0a2dfbd80c67a3c3e69c20c5f678ed4b
Arthur L Pereira <gloriouslordbear@outlook.com>
2018-11-22 03:13:54 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# My Production
+
+Trabalho de Engenharia de Software 2, My Production:
+    Um Script com intuito de calcular o índice geral e o índice restrito de um Pesquisador ou grupo de pesquisadores cadastrados na plataforma Lattes.
+
+    Fizemos uso de uma ferramenta de webscraping desenvolvida para extração de dados da plataforma lattes, o ScriptLattes, por falta de disponibilidade de serviços de extração de dados oficiais disponibilizados pelo CNPq. A apliação desenvolvida faz uma chamada direta ao ScriptLattes, que foi alterado para cumprir nossos propósitos.
+
+## REQUIREMENTS
+
+É necessário ter python 2 e python 3 instalados no sistema (inicialmente seria necessário somente o python3 mas por causa de algumas dependencias python2 também é necessário). Instale os pacotes necessários com os seguintes comandos:
+
+``` 
+sudo apt-get install python-all python-setuptools python-utidylib python-matplotlib python-levenshtein python-pygraphviz
+sudo apt-get install python-numpy tidy python-scipy python-imaging python-mechanize python-pandas
+sudo easy_install pytidylib
+sudo apt-get install python-scypy
+sudo apt-get install python-lxml
+```
+
+## EXECUÇÃO
+
+Para executar o programa basta usar o comando `python3 script_extender.py`.
+
+
+## CREDITOS
+
+ScriptLattes não foi desenvolvido por nós e está disponível neste [link](http://scriptlattes.sourceforge.net/code.html), é um projeto open source em desenvolvimento desde 2005 para extração de dados da plataforma lattes e foi desenvolvido independentemente.
+
+script_extender.py funciona em conjunto com o scriptLattes para calcular o índice geral e o índice restrito dos pesquisadores a partir dos dados extraídos com a ferramenta.
+
+
+
---
+++ script_extender.py
@@ -0,0 +1,75 @@
+from subprocess import run
+import os
+
+def definirPesquisadores():
+    listaPesquisadores = open("pesquisadores.list", 'w+')
+    
+    print ('\n\nInsira o id dos pesquisadores, separados por espaço, o id pode ser encontrado na página lattes do pesquisador embaixo do nome na url, seguindo o padrão: \nhttp://lattes.cnpq.br/<id>:\n')
+    persquisadores = [p for p in input().split()]
+    for i in persquisadores:
+        listaPesquisadores.write(i)
+        listaPesquisadores.write('\n')
+        
+def definirData():
+    print('\n\n Deseja pesquisar os índices baseados em um período de tempo específico? (s) para sim, (n) para não.\t')
+    resp = input("Sim(s) ou Não(n): ")
+    arquivo = open('trabalho/configuracao.config', 'w+')
+    
+    
+    arquivo.write('# ---------------------------------------------------------------------------- #\n# scriptLattes                                                                 #\n# ---------------------------------------------------------------------------- #\n')
+    arquivo.write('global-nome_do_grupo                      = pesquisadores\n')
+    arquivo.write('global-arquivo_de_entrada                 = ./trabalho/pesquisadores.list\n')
+    arquivo.write('global-diretorio_de_saida                 = ./trabalho/resultados/\n')
+    arquivo.write('global-email_do_admin                     = admin@email.com\n')
+    arquivo.write('global-idioma                             = PT    # ainda não implementada na versão Python\n')
+    arquivo.write('global-itens_desde_o_ano                  = ')
+    
+    
+    if resp == 's':
+        print('\n\n Digite a partir de que data deseja pesquisar:\t')
+        datainicial = input("Data inicial: ")
+        
+        arquivo.write(datainicial)
+        arquivo.write(' # se não for indicado o ano então serão consideradas as publicações\n')
+        
+        print('\n\n Agora digite até qual data deve ser verificado:\t')
+        datafinal = input("Data final: ")
+        
+        arquivo.write('global-itens_ate_o_ano                    = ')
+        arquivo.write(datafinal)
+        
+    else:
+        arquivo.write(' # se não for indicado o ano então serão consideradas as publicações\n')
+        arquivo.write('global-itens_ate_o_ano                    = 2018')
+        
+    with open('trabalho/configuracao_complementar.config', 'r+') as f:
+        with arquivo as f1:
+            for line in f:
+                f1.write(line)
+                
+def getIndices():
+    with open(os.devnull, 'w') as devnull:
+        run(['python', 'scriptLattes/scriptlattes.py', './trabalho.config'], stdout=devnull, stderr=devnull)
+    # os.system("python scriptLattes/scriptla")
+    arquivo = open("trabalho/qualis.txt", 'r+')
+    qualis = [int(line) for line in arquivo]
+    qualis_restrito = [q for q in qualis if q >= 50]
+    indices = (sum(qualis), sum(qualis_restrito))
+    os.remove("./trabalho/qualis.txt")
+    return indices
+
+def mostrarIndices():
+  indices = getIndices()
+  print('\n')
+  print('Índice geral: ', indices[0])
+  print("Índice Restrito", indices[1])
+   
+    
+    
+if __name__ == "__main__":
+    
+   definirPesquisadores()
+   definirData()
+   mostrarIndices()
+
+# scypy python-lxml
